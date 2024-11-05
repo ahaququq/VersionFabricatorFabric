376c55f4a7c58763f7cc2d44fb7f7d87ff766123
Ahaququq <ahaququq@outlook.com>
2024-11-05 08:50:35 +0100

    init

---
+++ src/client/java/org/github/ahaququq/versionFabricator/client/VersionFabricatorClient.java
@@ -0,0 +1,10 @@
+package org.github.ahaququq.versionFabricator.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class VersionFabricatorClient implements ClientModInitializer {
+
+	@Override
+	public void onInitializeClient() {
+	}
+}
---
+++ src/main/java/org/github/ahaququq/versionFabricator/VersionFabricator.java
@@ -0,0 +1,10 @@
+package org.github.ahaququq.versionFabricator;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VersionFabricator implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+	}
+}
